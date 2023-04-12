b14cef97f68895d22f3f133c7f1b27e17d3c1b03
GregoryPetrovLn <grishap96123@gmail.com>
2023-04-12 22:15:26 +0800

    created animation player function

--- index.js
+++ index.js
@@ -4,5 +4,27 @@ const c = canvas.getContext("2d");
 canvas.width = 64 * 16; //1024
 canvas.height = 64 * 9; //576  ratio
 
-c.fillStyle = "white";
-c.fillRect(0, 0, canvas.width, canvas.height);
+
+
+
+let y = 100;
+c.fillRect(100, y, 100, 100);
+let playerHeight = 100;
+let bottom = y + playerHeight;
+function animate() {
+  window.requestAnimationFrame(animate);
+
+  //playground
+  c.fillStyle = "white";
+  c.fillRect(0, 0, canvas.width, canvas.height);
+
+  //player
+  c.fillStyle = "red";
+  c.fillRect(100, y, 100, playerHeight);
+  if (bottom < canvas.height) {
+    y++;
+    bottom = y + 100;
+  }
+}
+
+animate();
