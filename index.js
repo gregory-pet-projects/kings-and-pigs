1ddaf24b44e7febc29950999ba369e6c7f4c5b8b
GregoryPetrovLn <grishap96123@gmail.com>
2023-04-13 00:22:41 +0800

    added key event to move by x position

--- index.js
+++ index.js
@@ -10,6 +10,17 @@ c.fillRect(100, y, 100, 100);
 //let bottom = y + playerHeight;
 const player = new Player();
 
+const keys = {
+  w: {
+    pressed: false,
+  },
+  a: {
+    pressed: false,
+  },
+  d: {
+    pressed: false,
+  },
+};
 function animate() {
   window.requestAnimationFrame(animate);
 
@@ -17,16 +28,55 @@ function animate() {
   c.fillStyle = "white";
   c.fillRect(0, 0, canvas.width, canvas.height);
 
+  player.velocity.x = 0;
+  if (keys.d.pressed) {
+    player.velocity.x = 1;
+  } else if (keys.a.pressed) {
+    player.velocity.x = -1;
+  }
+
   player.draw();
   player.update();
 }
 
 animate();
 
+function jump() {
+  if (player.velocity.y === 0) {
+    player.velocity.y = -20;
+  }
+}
+
+function movePlayerToLeft(keydown = false) {
+  keys.a.pressed = keydown;
+}
+function movePlayerToRight(keydown = false) {
+  keys.d.pressed = keydown;
+}
+
 window.addEventListener("keydown", (event) => {
   switch (event.key) {
     case "w":
-      player.velocity.y = -10;
+      jump();
+      break;
+    case "a":
+      movePlayerToLeft(true);
+      break;
+    case "d":
+      movePlayerToRight(true);
+      break;
+    default:
+      "";
+  }
+});
+
+window.addEventListener("keyup", (event) => {
+  switch (event.key) {
+    case "a":
+      movePlayerToLeft();
+      break;
+    case "d":
+      movePlayerToRight();
       break;
     default:
       "";
