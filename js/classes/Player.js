1d15237acc7b0be2d700bcbfbdb4609c879f3e7b
GregoryPetrovLn <grishap96123@gmail.com>
2023-04-15 22:07:24 +0800

    added comments

--- js/classes/Player.js
+++ js/classes/Player.js
@@ -40,37 +40,53 @@ class Player {
   }
 
   checkForCollisions(axis) {
+    // Check if axis is vertical or not
     const isVertical = axis === VERTICAL_AXIS;
+    // Set buffer based on axis
     const buffer = isVertical ? BUFFER : EPSILON;
 
+    // Loop through all collision blocks
     for (let i = 0; i < this.collisionBlocks.length; i++) {
       const collisionBlock = this.collisionBlocks[i];
-      //if collision exist
+
+      // Check if collision exists with current block
       if (
-        this.position.x <= collisionBlock.position.x + collisionBlock.width && //right side
-        this.position.x + this.width >= collisionBlock.position.x && //left side
-        this.position.y + this.height >= collisionBlock.position.y && //bottom
-        this.position.y <= collisionBlock.position.y + collisionBlock.height //top
+        this.position.x <= collisionBlock.position.x + collisionBlock.width && // Check if object is on right side of block
+        this.position.x + this.width >= collisionBlock.position.x && // Check if object is on left side of block
+        this.position.y + this.height >= collisionBlock.position.y && // Check if object is below block
+        this.position.y <= collisionBlock.position.y + collisionBlock.height // Check if object is above block
       ) {
+        // If axis is vertical
         if (isVertical) {
+          // Check if object is moving up
           if (this.velocity.y < 0) {
+            // Stop upward movement
             this.velocity.y = 0;
+            // Move object to bottom of block
             this.position.y =
               collisionBlock.position.y + collisionBlock.height + buffer;
             break;
           }
+          // Check if object is moving down
           if (this.velocity.y > 0) {
+            // Stop downward movement
             this.velocity.y = 0;
+            // Move object to top of block
             this.position.y = collisionBlock.position.y - this.height - buffer;
             break;
           }
         } else {
+          // If axis is horizontal
+          // Check if object is moving left
           if (this.velocity.x < 0) {
+            // Move object to right of block
             this.position.x =
               collisionBlock.position.x + collisionBlock.width + buffer;
             break;
           }
+          // Check if object is moving right
           if (this.velocity.x > 0) {
+            // Move object to left of block
             this.position.x = collisionBlock.position.x - this.width - buffer;
             break;
           }
